a6880a34986068e33579dbac129685e6026af0c2
littlebear789 <3dhlinhardt@gmail.com>
2025-03-04 18:45:27 -0800

    update

--- 5700FRC2025SwervePathPlanner/src/main/java/frc/robot/RobotContainer.java
+++ 5700FRC2025SwervePathPlanner/src/main/java/frc/robot/RobotContainer.java
@@ -33,7 +33,7 @@ public class RobotContainer {
     private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
     private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
 
-    /* Setting up bindings for necessary control of the swerve drive platform */
+    /* Setting up bindings for necessary control of the swerve drive platform. */
     private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
             .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
             .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
@@ -51,7 +51,7 @@ public class RobotContainer {
     /* Elevator */
     public final ElevatorSubsystem elevator = new ElevatorSubsystem();
 
-    
+
     /* Arm */
     public final ArmSubsystem arm = new ArmSubsystem();
 
